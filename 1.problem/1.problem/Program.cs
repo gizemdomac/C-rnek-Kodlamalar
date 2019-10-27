3bd4a2204bba436a374d7ad88b6fa4425a0acce8
gizenk <domacgizem@gmail.com>
2019-10-27 18:36:23 +0300

    csharp

---
+++ 1.problem/1.problem/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dizideki eleman sayisini girin:");
+            int boyut = Convert.ToInt32(Console.ReadLine());
+            int[] dizi = new int[boyut];
+
+            for (int i = 0; i <Math.Pow(2,boyut); i++)
+            {
+                Console.Write(i + ".elemanı giriniz: ");
+                dizi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            string s = "";
+            int max = 0;
+            int ara_deger;
+
+            for (int i = 0; i < boyut; i++)
+            {
+                int b = i;
+               
+                s = "(";
+                ara_deger = 0;
+                int sum = 0;
+                for (int j = 0; j < boyut; j++)
+                {
+                    
+                    if ((b & 1) == 1)
+                    {
+                        if (j - ara_deger > 1)
+                        {
+                            s += dizi[j].ToString() + ",";
+                            sum = sum + dizi[j];
+                            ara_deger = j;
+                        }
+                    
+                    }
+                    b = b >> 1;
+                }
+                
+                s += "),  ";
+                Console.Write(s+"=>");
+                Console.WriteLine(sum.ToString());
+
+                if(max<sum)
+                {
+                    max = sum;
+                }
+            }
+            Console.Write("En büyük toplam:");
+            Console.WriteLine(max.ToString());
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 2.problem/2.problem/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input1, input2 ,division ;
+           
+            Console.WriteLine("Birinci sayiyi girin:");
+            input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("İkinci sayiyi girin:");
+            input2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bölmek istediğiniz sayiyi girin:");
+            division = Convert.ToInt32(Console.ReadLine());
+
+            int max,min;
+           
+            if(input2>=input1)
+            {
+                max = input2;
+                min = input1;
+            }
+            else 
+            {
+                max = input1;
+                min = input2;
+            }
+
+            int sum = 0;
+
+            for(int i=min; i<=max; i++)
+            {
+     
+                if(i%division==0)
+                {
+                    sum =sum+i; 
+                }
+            }
+
+
+            Console.Write("Sonuç:");
+            Console.WriteLine(sum.ToString());
+            Console.ReadLine();
+            }
+        
+        }
+
+    }
+
